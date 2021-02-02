0d46bb467c74dd875370385f5f0b9b827247ffd7
Dario Zubaray <dzubaray@gmail.com>
2021-02-02 11:38:33 -0300

    Adding query annotation in order to let spring update todo

--- src/main/java/com/zubaray/todo/repository/TodoRepository.java
+++ src/main/java/com/zubaray/todo/repository/TodoRepository.java
@@ -1,6 +1,9 @@
 package com.zubaray.todo.repository;
 
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.zubaray.todo.models.Todo;
@@ -8,7 +11,11 @@ import com.zubaray.todo.models.Todo;
 @Repository
 public interface TodoRepository extends CrudRepository<Todo, Long> {
 
-    Todo updateTodoMessage(Long id, Todo newTodo);
-    Todo updateTodo(Long id, String newMessage);
+    @Modifying(clearAutomatically = true)
+    @Query("UPDATE Todo t SET t.message =:#{#newTodo.message}, t.finished =:#{#newTodo.finished} where t.id =:id")
+    public void updateTodoMessage(@Param("id") Long id, @Param("newTodo") Todo newTodo);
+    @Modifying(clearAutomatically = true)
+    @Query("UPDATE Todo t SET t.message =:newMessage where t.id =:id")
+    public void updateTodo(@Param("id") Long id, @Param("newMessage") String newMessage);
 
 }
