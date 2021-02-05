a1f95e687c02009b553762086aee8de5f8be98bd
Dario Zubaray <dzubaray@gmail.com>
2021-02-04 23:49:25 -0300

    adding swagger

---
+++ src/main/java/com/zubaray/todo/config/SpringFoxConfig.java
@@ -0,0 +1,11 @@
+package com.zubaray.todo.config;
+
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.oas.annotations.EnableOpenApi;
+
+@Configuration
+@EnableOpenApi
+public class SpringFoxConfig {
+
+}
